c8e44827b6b616d47f7027dff1905f58ee213e51
Christophe PEREZ <cperez@solocal.com>
2020-05-18 18:19:37 +0200

    last good

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <a class="navbar-brand" href="#">Share RPG</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
           aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -11,7 +11,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <router-link class="nav-link" to="/">Home</router-link> |
+              <a class="nav-link" href="#">Home</a>
             </li>
             <li class="nav-item">
               <router-link class="nav-link" to="/about">About</router-link> |
@@ -22,7 +22,33 @@
           </ul>
         </div>
       </div>
-    </nav>
+    </nav> -->
+
+    <!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container">
+  <a class="navbar-brand" href="#">Share RPG</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <router-link class="nav-link" to="/">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/contact">Contact</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/about">About</router-link>
+      </li>
+    </ul>
+  </div>
+  </div>
+</nav>
+
+
+
     <router-view/>
   </div>
 </template>
@@ -33,7 +59,7 @@
   -moz-osx-font-smoothing: grayscale;
 }
 
-#navbarResponsive {
+#navbarNav {
   a:focus {
     font-weight: bold;
     &.router-link-exact-active {
@@ -41,12 +67,11 @@
     }
   }
 
+  height: 30px;
+
   a:visited {
       font-weight: normal;
       color: #626d77;
-
   }
-
-
 }
 </style>
