d12435f511b3ac7d1bb3989b250e6229099e2a72
trotroyanas <trotroyanas@gmail.com>
2020-05-20 13:57:00 +0200

    add new css

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,8 @@
 <template>
   <div id="app">
-    <!-- Navigation -->
-    <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="#">Share RPG</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-          aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/about">About</router-link> |
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/contact">Contact</router-link> |
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>-->
-
-    <!-- Navigation -->
-    <header class="header fixed-top">
-      <div class="container">
-        <a class="logo" href="#">Share RPG</a>
-        <input class="menu-btn" type="checkbox" id="menu-btn" />
-        <label class="menu-icon" for="menu-btn">
-          <span class="navicon"></span>
-        </label>
-        <ul class="menu">
-          <li>
-            <router-link class="nav-link" to="/">Home</router-link>
-          </li>
-          <li>
-            <router-link class="nav-link" to="/about">About</router-link>
-          </li>
-          <li>
-            <router-link class="nav-link" to="/contact">Contact</router-link>
-          </li>
-        </ul>
-      </div>
-    </header>
-
+    <router-link class="nav-link" to="/about">about</router-link>|
+    <router-link class="nav-link" to="/login">login</router-link>|
+    <router-link class="nav-link" to="/contact">Contact</router-link>
     <router-view />
   </div>
 </template>
@@ -54,5 +11,21 @@
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+#navbarNav {
+  a:focus {
+    font-weight: bold;
+    &.router-link-exact-active {
+      color: #fff;
+    }
+  }
+
+  height: 30px;
+
+  a:visited {
+    font-weight: normal;
+    color: #626d77;
+  }
 }
 </style>
