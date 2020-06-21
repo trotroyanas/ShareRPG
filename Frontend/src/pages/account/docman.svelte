abbfb1a38c5513e515db4ba932e08d871b9b820c
trotroyanas <trotroyanas@gmail.com>
2020-06-21 18:52:39 +0200

    add upload files with del button

--- Frontend/src/pages/account/docman.svelte
+++ Frontend/src/pages/account/docman.svelte
@@ -14,11 +14,53 @@
   if (!_.isEmpty(isConnect) || isConnect === false) {
     $goto("/login");
   }
+
+  let files = [];
+  function listFiles(e) {
+    $: files = e.target.files;
+  }
+
+  function del(key) {
+    console.log("key:", key);
+
+    let ff = files;
+    let nn = Array.from(ff);
+    console.log(nn);
+    nn.splice(key, 1);
+    console.log(nn);
+    files = nn;
+  }
 </script>
 
 <style lang="scss">
   .pdt10 {
     padding-top: 10px;
+  }
+
+  .dd {
+    text-align: center;
+  }
+
+  /* on personnalise le label comme on veut */
+  .label-file {
+    cursor: pointer;
+    color: #00b1ca;
+    font-weight: bold;
+    border: 1px solid #25a5c4;
+    padding: 15px;
+  }
+  .label-file:hover {
+    color: #25a5c4;
+  }
+  /* et on masque le input */
+  .input-file {
+    display: none;
+  }
+
+  .pup {
+    text-align: left;
+    padding-left: 10px;
+    border: 1px solid red;
   }
 </style>
 
@@ -32,8 +74,24 @@
           Documents Manager
         </h2>
       </div>
+      <!-- Nous avons ici notre label et l'input afférent -->
 
-      <div id="drag-drop-area" />
+      <div class="dd">
+        <label for="file" class="label-file">Choisir une image</label>
+        <input
+          id="file"
+          class="input-file"
+          type="file"
+          multiple
+          on:change={listFiles} />
+
+        {#each files as d, i}
+          <div class="pup">
+            {d.name}, {d.size}, {d.type}
+            <input type="button" on:click={() => del(i)} value="del" />
+          </div>
+        {/each}
+      </div>
     </div>
   </div>
 </div>
